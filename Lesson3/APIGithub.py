0102c25f0f59dcadd845bcbee0a992cfe5b68693
Ly Adrien <lyadrien@Lys-MacBook-Pro.local>
2018-10-19 12:31:20 +0200

    V2 de lesson3

--- Lesson3/APIGithub.py
+++ Lesson3/APIGithub.py
@@ -1,33 +1,62 @@
 # coding: utf-8
 
-# import re
-# import unittest
 import json
-import argparse
+import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-from jq import jq
-import pprint
 
+# Parameters
+acces_token = ""
 url_page = "https://gist.github.com/paulmillr/2657075"
-res = requests.get(url_page)
-if res.status_code == 200:
-    html_doc = res.text
-    soup = BeautifulSoup(html_doc, "html.parser")
+head = {'Authorization': 'token {}'.format('')}
+url_get = "https://api.github.com/users/"
+df = pd.DataFrame(columns = ['contributors', 'number_of_repos', 'average_stargazers'])
 
-table = soup.find("table").find('tbody')
-rows = table.find_all('tr')
-list_cont = []
-for row in rows:
-    cont = row.find_next("td").text
-    cont = cont.split(" ")
-    list_cont.append(cont[0])
-print(list_cont)
 
-token = "dbf09a3d5e0b47b313e454c5a33069c7870ef712"
-head = {'Authorization': 'adrienly' + token}
-url_api = f"https://api.github.com/search/users/fabpot/repo"
-r = requests.get(url_api, headers = head, verify = False)
-data = r.json()
-pprint.pprint(r)
+def build_soup(url):
+    res = requests.get(url)
+    if res.status_code == 200:
+        html_doc = res.content
+        soup = BeautifulSoup(html_doc, "html.parser")
+        return soup
+
+def _get_top_contributors(soup):
+    table = soup.find("table").find('tbody')
+    rows = table.find_all('tr')
+    list_cont = []
+    for row in rows:
+        cont = row.find_next("td").text
+        cont = cont.split(" ")
+        list_cont.append(cont[0])
+    return list_cont
+
+def get_mean_stars_users(listcontrib):
+    #Initialisation des variables
+    dico_mean_stars = {}
+    list_stars_mean = []
+
+    dico_mean_stars["login"] = listcontrib 
+    for i in range(len(listcontrib)):
+        sum_stars = 0
+        mean_stars = 0
+        get_repo = requests.get("https://api.github.com/users/" + listcontrib[i] + "/repos", headers=head)
+        if get_repo.status_code == 200:
+            get_json = json.loads(get_repo.content)
+            for j in range(len(get_json)):
+                sum_stars += get_json[j].get("stargazers_count")
+            mean_stars = sum_stars/len(get_json)
+            list_stars_mean.append(mean_stars)
+            dico_mean_stars["mean"] = list_stars_mean
+    df_users_stars_mean = pd.DataFrame.from_dict(dico_mean_stars)
+    # print(df_users_stars_mean)
+    return df_users_stars_mean
+
+def main():
+    soup = build_soup(url_page)
+    df['contributors'] = _get_top_contributors(soup)
+    df['average_stargazers'] = get_mean_stars_users(_get_top_contributors(soup))
+
+
+
+
 
